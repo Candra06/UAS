ba1a76dab82f8aa7679223c846b3a87c2038698b
candra06 <candrabiyu4@gmail.com>
2022-06-14 18:22:51 +0700

    set config

--- config/app.php
+++ config/app.php
@@ -1,10 +1,10 @@
 <?php
 // define('BASEURL', "http://" . $_SERVER['SERVER_NAME'] . '/' . basename(__DIR__) . '/');
-define('BASEURL', "http://" . $_SERVER['SERVER_NAME'] . '/UAS');
-$host = "localhost";
-$user = "root";
-$password = "bismillah5758-";
-$database = "uas";
+define('BASEURL', "http://pbw.ilkom.unej.ac.id/ifd/ifd172410103012");
+$host = "proxy.ilkom.unej.ac.id";
+$user = "172410103012";
+$password = "secret";
+$database = "uas_172410103012";
 
 $con = mysqli_connect($host, $user, $password, $database);
 
