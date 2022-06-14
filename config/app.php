82998c25b0a69a7e966d95e065891e5767e1c3bf
candra06 <candrabiyu4@gmail.com>
2022-06-14 18:37:12 +0700

    new config

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 // define('BASEURL', "http://" . $_SERVER['SERVER_NAME'] . '/' . basename(__DIR__) . '/');
 define('BASEURL', "http://pbw.ilkom.unej.ac.id/ifd/ifd172410103012/uas_172410103012");
-$host = "proxy.ilkom.unej.ac.id";
+$host = "localhost";
 $user = "172410103012";
 $password = "secret";
 $database = "uas_172410103012";
