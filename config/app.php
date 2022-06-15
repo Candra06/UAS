bd0ce52df8fad2be948c4613600349be7cd1ae92
candra06 <candrabiyu4@gmail.com>
2022-06-15 14:59:59 +0700

    perbaikan add barang

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 // define('BASEURL', "http://" . $_SERVER['SERVER_NAME'] . '/' . basename(__DIR__) . '/');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 define('BASEURL', "http://pbw.ilkom.unej.ac.id/ifd/ifd172410103012/uas_172410103012");
 $host = "localhost";
 $user = "172410103012";
