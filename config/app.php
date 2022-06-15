5fb04ad0371b25e14fc023da0d7e382178a22e8e
candra06 <candrabiyu4@gmail.com>
2022-06-15 15:02:19 +0700

    perbaikan add barang

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ define('BASEURL', "http://pbw.ilkom.unej.ac.id/ifd/ifd172410103012/uas_172410103
 $host = "localhost";
 $user = "172410103012";
 $password = "secret";
-$database = "uas_172410103012";
+$database = "uas172410103012";
 
 $con = mysqli_connect($host, $user, $password, $database);
 
